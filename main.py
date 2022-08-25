a74f64ca3c86c5baab11beb52bc49127bf8481b5
evanptaylor <eevantaylorr@gmail.com>
2022-08-25 17:57:10 -0400

    added buy and hold strategy plus basic output and graph

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import backtrader as bt
 import datetime
+from strategy_buyhold import BuyHold
 
 #instatiate cerebro engine
 engine = bt.Cerebro()
@@ -13,4 +14,15 @@ data = bt.feeds.YahooFinanceCSVData(
 )
 #add data to engine
 engine.adddata(data)
+#add a strategy
+engine.addstrategy(BuyHold)
 
+#portfolio start value (before strategy is run) 
+print(f'Portfolio value (start): {engine.broker.getvalue()}')
+
+#run the engine
+engine.run()
+print(f"Portfolio value (end): {engine.broker.getvalue()}")
+
+#plot results to a graph
+engine.plot()
