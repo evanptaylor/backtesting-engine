17fd9f3fc8f476be0f3480e7abff52399dc340df
evanptaylor <eevantaylorr@gmail.com>
2022-08-25 18:15:20 -0400

    updated with more info in output

--- strategy_buyhold.py
+++ strategy_buyhold.py
@@ -2,14 +2,20 @@ import backtrader as bt
 import math
 
 class BuyHold(bt.Strategy):
+    params = (('ticker', 'SPY'), ('order_percentage', 1))
+
+    def log(self, txt, dt=None):
+        dt = dt or self.datas[0].datetime.date(0)
+        print('%s, %s' % (dt.isoformat(), txt))
+
     def __init__(self):
         self.order = None
     
     def next(self):
         if self.order == None:
-            size = math.floor(self.broker.cash/self.data.close)
+            pos_size = (self.params.order_percentage * self.broker.cash)
+            size = math.floor(pos_size/self.data.close)
             self.order = self.buy(size=size)
-            print(f"BUY: {size} shares of SPY @ {self.data.close} ({self.datas[0].datetime.date(0)}) ")
-        
+            self.log(f"BUY: {size} shares of SPY @ {self.data.close[0]}")
 
 
